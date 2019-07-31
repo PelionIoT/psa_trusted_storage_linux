8bfd92a3cbdc56ad74c30de4d1279670614543ed
Simon D Hughes <simon.hughes@arm.com>
2019-07-31 15:16:42 +0100

    Fix file object storage location, flags and storage prefix configuration. (#4)
    * PSA Storage: Support storing ITS and PS file objects in separate directories.
    
    The following provides more information on this commit:
    - The storage directories are distinct for ITS and PS file objects.
      For example, the ITS file objects are stored in
      PSA_STORAGE_FILE_C_STORAGE_PREFIX"/its", and the PS file objects are stored in
      PSA_STORAGE_FILE_C_STORAGE_PREFIX"/ps".
    - This commit corrects the implementation so the file objects stored via the
      ITS/PS interfaces are stored in the respective directories.
    
    * PSA Storage: Fix PSA_STORAGE_FLAG_xxx implementations.
    
    The following provides more information on this commit:
    - The PSA_STORAGE_FLAG_WRITE_ONCE behaviour is implemented i.e. a file object
      created with this flag cannot be deleted or updated.
    - File objected are stored using an ecryptfs cipher directory, so all files are
      stored with confidentiality. If the PSA_STORAGE_FLAG_NO_CONFIDENTIALITY is
      set on creation, then the flag is not set in the file object meta-data stored
      together with the data. The get_info() retrieved info.flags therefore reports
      the flag being not set. Applying a greater level of security than requested is
      permitted by the PSA storage specification.
    - The psa-storage-example-app has been updated to include a new test to check the
      above behaviours.
    
    * PSA Storage: Add PSA_STORAGE_FILE_C_STORAGE_PREFIX preprocessor guard.
    
    The following provides more information on this commit:
    - A preprocessor guard is added to PSA_STORAGE_FILE_C_STORAGE_PREFIX in
      config.h so that
        CFLAGS="-DPSA_STORAGE_FILE_C_STORAGE_PREFIX='\"${TEST_DIR}\"'"
      can be specified to make, for example.

--- lib/internal_trusted_storage.c
+++ lib/internal_trusted_storage.c
@@ -5,7 +5,7 @@
  */
 #include "config.h"
 #include "psa/internal_trusted_storage.h"
-#include "psa/protected_storage.h"
+#include "common_storage.h"
 
 psa_status_t psa_its_set(psa_storage_uid_t uid,
                          size_t data_length,
@@ -13,7 +13,7 @@ psa_status_t psa_its_set(psa_storage_uid_t uid,
                          psa_storage_create_flags_t create_flags)
 {
 
-    return psa_ps_set(uid, data_length, p_data, create_flags);
+    return psa_cs_set(uid, data_length, p_data, create_flags, PSA_CS_API_ITS );
 
 }
 
@@ -23,17 +23,17 @@ psa_status_t psa_its_get(psa_storage_uid_t uid,
                          void *p_data,
                          size_t *p_data_length)
 {
-    return psa_ps_get(uid, data_offset, data_size, p_data, p_data_length);
+    return psa_cs_get(uid, data_offset, data_size, p_data, p_data_length, PSA_CS_API_ITS );
 }
 
 psa_status_t psa_its_get_info(psa_storage_uid_t uid,
                               struct psa_storage_info_t *p_info)
 {
-    return psa_ps_get_info(uid, p_info);
+    return psa_cs_get_info(uid, p_info, PSA_CS_API_ITS );
 }
 
 psa_status_t psa_its_remove(psa_storage_uid_t uid)
 {
-    return psa_ps_remove(uid);
+    return psa_cs_remove(uid, PSA_CS_API_ITS );
 }
 
