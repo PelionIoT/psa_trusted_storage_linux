0641e0421c3edc5dbae6fc460a32c75f034d3929
Simon Hughes <simon.hughes@arm.com>
2020-01-09 14:27:16 +0000

    SQ: Update <topdir>/test/psa_test_case_201.sh with shellcheck fixes.

--- test/psa_test_case_201.sh
+++ test/psa_test_case_201.sh
@@ -1,4 +1,4 @@
-# !/bin/bash
+#!/bin/bash
 #
 ###############################################################################
 # Copyright (c) 2019 Arm Limited and Contributors. All rights reserved.
@@ -9,7 +9,7 @@
 # multi-thread support. The script runs multiple concurrent instances of the
 # PSA Storage test application as different users. This tests that multiple
 # concurrent execution contexts using the PSA Storage library and sharing the
-# storage area co-exist with one another without one process causing error for
+# storage area co-exist with one another without one process causing errors for
 # another process.
 #
 # This script is intended to be run as part of x86 testing where an x86
@@ -26,13 +26,13 @@ PSA_TEST_201_USER2=testuser2
 PSA_CMD_ID=id
 # psa-storage-example-app needs to be on the PATH
 PSA_STORAGE_TEST_BIN=psa-storage-example-app
-PSA_CMD_TEST_APP=`which ${PSA_STORAGE_TEST_BIN}`
+PSA_CMD_TEST_APP=$(which ${PSA_STORAGE_TEST_BIN})
 
 
 # Check application is found
 _psa_return_status=1
 #if [ "${PSA_CMD_TEST_APP}"+ == ""+ ]; then
-if [ -z ${PSA_CMD_TEST_APP} ]; then
+if [ -z "${PSA_CMD_TEST_APP}" ]; then
     echo "Error: ${PSA_STORAGE_TEST_BIN} not found on the path. Set PATH to include test binary directory."
     exit ${_psa_return_status}
 fi
@@ -59,15 +59,16 @@ chmod -R o+w test
 
 # Run 2 instances of the test application binary in parallel, repeating multiple times.
 _psa_return_status=0
+# shellcheck disable=SC2034
 for i in 0 1 2 3 4 5 6 7 8 9
 do
     # Run n copies of the test application simultaneously to test multi-process support.
     # Run the first instance in the background
-    sudo runuser -u ${PSA_TEST_201_USER1} -- ${PSA_CMD_TEST_APP} -v &
+    sudo runuser -u ${PSA_TEST_201_USER1} -- "${PSA_CMD_TEST_APP}" -v &
     _user1_pid=$!
 
     # run the second instance in the foreground so the script doesnt terminate
-    sudo runuser -u ${PSA_TEST_201_USER2} -- ${PSA_CMD_TEST_APP} -v &
+    sudo runuser -u ${PSA_TEST_201_USER2} -- "${PSA_CMD_TEST_APP}" -v &
     _user2_pid=$!
 
     wait ${_user1_pid}
